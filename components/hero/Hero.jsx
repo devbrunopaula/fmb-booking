720de647d2169b5a2075fcdd972b645d2058d869
Bruno <devbrunopaula@gmail.com>
2021-11-24 12:45:46 -0500

    Bug iphone new update screen size

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import styles from './hero.module.scss'
-import {BsFillCaretDownFill} from 'react-icons/bs'
+
 export default function Hero() {
 	return (
 		<div className={`h-screen ${styles.hero}`}>
-			<div className=' h-screen flex justify-center items-center'>
-				<div className='flex flex-col items-center'>
+			<div
+				className={`h-screen flex justify-center items-start pt-36 lg:items-center lg:pt-0 ${styles.test1}`}
+			>
+				<div className={`flex flex-col items-center ${styles.test2}`}>
 					<img
 						className={`hidden ${styles.logo}`}
 						src='https://fixmyboatusa.com/wp-content/uploads/2021/11/FMB-fullcolor-1.png'
 						alt=''
 					/>
-					<h1 className='text-5xl py-5 md:text-8xl font-bold text-gray-900 md:text-center'>
+					<h1 className='text-5xl py-5 md:text-8xl font-bold text-gray-900 text-center'>
 						On-Demand Mobile
 					</h1>
 					<h3 className='text-6xl  font-light text-gray-200 italic'>
@@ -33,7 +35,7 @@ export default function Hero() {
 			</div>
 			<div className={`${styles.footer} h-52 lg:h-20`}>
 				<div
-					className={`container flex flex-col justify-center mx-auto px-4 items-center h-full lg:flex-row  `}
+					className={`container flex flex-col justify-center mx-auto px-4 items-center h-scree lg:flex-row  `}
 				>
 					<div className='lg:flex-1'>
 						<p
